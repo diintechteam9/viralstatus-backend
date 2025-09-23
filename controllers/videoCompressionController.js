12db9db1b31fac4cc5203c08967434da3f453593
diintechteam9 <diintechteam9@gmail.com>
2025-09-23 16:48:38 +0530

    video to reels

--- controllers/videoCompressionController.js
+++ controllers/videoCompressionController.js
@@ -45,7 +45,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 500 * 1024 * 1024, // 500MB limit
+    fileSize: 5 * 1024 * 1024 * 1024, // 5GB limit
   },
   fileFilter: (req, file, cb) => {
     if (isSupportedFormat(file.originalname)) {
