51e7cbc90197033e68753f797c50dfbcecc4812a
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-03-28 15:26:39 +0530

    update

--- config/r2.js
+++ config/r2.js
@@ -8,6 +8,8 @@ const r2Client = new S3Client({
     accessKeyId: process.env.R2_ACCESS_KEY,
     secretAccessKey: process.env.R2_SECRET_KEY,
   },
+  requestChecksumCalculation: 'WHEN_REQUIRED',
+  responseChecksumValidation: 'WHEN_REQUIRED',
 });
 
 const BUCKET_NAME = process.env.R2_BUCKET;
